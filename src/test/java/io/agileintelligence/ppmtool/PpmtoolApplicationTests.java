f31aa7b837667f3dc8df9452beab510d7f66f165
Manideepakreddi <manideepreddi007@gmail.com>
2021-12-30 12:14:04 -0500

    heroku changes

--- src/test/java/io/agileintelligence/ppmtool/PpmtoolApplicationTests.java
+++ src/test/java/io/agileintelligence/ppmtool/PpmtoolApplicationTests.java
@@ -1,4 +1,4 @@
-package io.agileintelligence.ppmtool;
+/** package io.agileintelligence.ppmtool;
 
 import org.junit.Test;
 import org.junit.runner.RunWith;
@@ -13,4 +13,4 @@ public class PpmtoolApplicationTests {
     public void contextLoads() {
     }
 
-}
+} */
