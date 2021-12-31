abb029371e5313897032675c3ef4d639fd2f71ee
Manideepakreddi <manideepreddi007@gmail.com>
2021-12-31 09:11:38 -0500

    react static file add

---
+++ src/main/resources/static/precache-manifest.86145b863dc734db0ca5a59c79deffaa.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "bad7ba05a6e9af2b789325f64c7df9a7",
+    "url": "/index.html"
+  },
+  {
+    "revision": "1f485be4a73d961e06b7",
+    "url": "/static/css/2.429e79c4.chunk.css"
+  },
+  {
+    "revision": "6bb8e98df73bb7dccc30",
+    "url": "/static/css/main.74587876.chunk.css"
+  },
+  {
+    "revision": "1f485be4a73d961e06b7",
+    "url": "/static/js/2.6a922194.chunk.js"
+  },
+  {
+    "revision": "dac7221200a2440de73372b0e56ca4ba",
+    "url": "/static/js/2.6a922194.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "ee42faf5777b718b92a8",
+    "url": "/static/js/3.0b96331c.chunk.js"
+  },
+  {
+    "revision": "6bb8e98df73bb7dccc30",
+    "url": "/static/js/main.3403fe7c.chunk.js"
+  },
+  {
+    "revision": "82850086085c77dab2d1",
+    "url": "/static/js/runtime-main.26562069.js"
+  }
+]);
